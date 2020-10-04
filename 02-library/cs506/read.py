e542bd67c87092233295ee69a4e246610b003144
ziyuzeng <zyzeng7@gmail.com>
2020-10-03 23:47:39 -0400

    Adding existing file

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,18 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, newline='') as csvfile:
+        csvread = csv.reader(csvfile, delimiter =' ')
+        matrix = []
+        for row in csvread:
+            newrow= []
+            for r in row[0].split(','):
+                if r.isnumeric():
+                    newrow +=[int(r)]
+                else:
+                    newrow += [r]
+                matrix +=[newrow]
+        return matrix
+
+
+    
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,58 @@
+import math
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if (len(x)==0 or len(y)==0):
+        return ValueError("Vector cannot be empty!")
+    if (len(x)!= len(y)):
+        return ValueError("Vector must be in the same dimension!")
+    sum = 0.
+    entdis = 0.
+    entdis_2 = 0.
+    for i in range(len(x)):
+        entdis = abs(x[i]-y[i])
+        entdis_2 = entdis**2
+        sum +=entdis_2
+    return (sum**0.5)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if(len(x)==0 or len(y)==0):
+        return ValueError("Vectors cannot be empty!")
+    if (len(x)!=len(y)):
+        return ValueError("Vectors must be in the same dimension!")
+    sum = 0.
+    entdis = 0.
+    for i in range(len(x)):
+        entdis = abs(x[i]-y[i])
+        sum += entdis
+    return sum   
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (len(x)==0 or len(y)==0):
+        return ValueError("Vector cannot be empty!")
+    if (len(x)!=len(y)):
+        return ValueError("Vector must be in the same dimension!")
+    
+    cap = set(x).intersection(set(y))
+    cup = set(x).union(set(y))
+    return 1-((abs(len(cap))))/(abs(len(cup)))   
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
-
+    if (len(x)==0 or len(y)==0):
+        return ValueError("Vector cannot be empty!")
+    if (len(x)!=len(y)):
+        return ValueError("Vector must be in the same dimension!")
+    
+    normx = 0.
+    normxsq = 0.
+    normy = 0.
+    normysq = 0.
+    dorprdc = 0.
+    for i in range(len(x)):
+        normxsq += x[i]**2
+        normysq += y[i]**2
+        normx += normxsq**.5
+        normy += normysq**.5
+        dotprodc += x[i]*y[i]
+    return (dotproc/(normx*normy)
+    
 # Feel free to add more
